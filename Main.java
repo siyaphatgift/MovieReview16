68fe32b30fa1aa448c1e463bd5e2a5813bae3ac2
PRP <peerapat.tri@mail.kmutt.ac.th>
2017-04-30 22:24:19 +0700

    Add main class

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
+public class Main {
+  public static void main(String args[]) throws IOException {
+    File reviewFile = new File("movieReviews.txt");
+    Scanner keyboard = new Scanner(System.in);
+    int reviewScore;
+    String reviewText;
+    String review;
+    double check = 0;
+    HashTable table = new HashTable(20000);
+    System.out.println("enter a review -- Press return to exit:");
+    review = keyboard.nextLine();
+    String[] arr = review.split(" ");
+    for (String word : arr) {
+      Scanner reviewScanner = new Scanner(reviewFile);
+      word = (" " + word + " ").toLowerCase();
+      while (reviewScanner.hasNext()) {
+        reviewScore = reviewScanner.nextInt();
+        reviewText = reviewScanner.nextLine().toLowerCase();
+        if (reviewText.contains(word)) {
+          table.put(reviewText, reviewScore);
+          check = table.getAverage(reviewText);
+          
+        }
+        
+      }
+      
+    }
+    
+    System.out.println("The review has an average value of " + check );
+    
+    if (check > 2) {
+      
+      System.out.println("Positive Sentiment");
+      
+    } else {
+      
+      System.out.println("Negative Sentiment");
+      
+    }
+    
+  }
+  
+}
