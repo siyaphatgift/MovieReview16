7e504b4805a0818bf6ae9a9297fd8767cd6370ad
PRP <peerapat.tri@mail.kmutt.ac.th>
2017-05-02 11:57:12 +0700

    Final HashTable

--- HashTable.java
+++ HashTable.java
@@ -1,61 +1,46 @@
 import java.util.*;
 
 public class HashTable {
-  //List<WordEntry> wordEntryList = new ArrayList();
-  private WordEntry []word;
+  
+  private WordEntry[] Word;
   private int size;
-  
-  public HashTable(int s)
-  {
-    this.size = s;
-    word = new WordEntry[s];
+  public HashTable(int size) {
+    this.size = size;
+    Word = new WordEntry[size];
   }
   
-  public int computeHash(String s) {
-    //String 
-    int x = 1;
-    for (WordEntry a: word) {
-      if(a.getWord().equals(s)) {
-        x++;
-      }
+  public int computeHash(String reviewText) {
+    int x = 2;
+    reviewText = reviewText.toLowerCase();
+    for (int temp = 0; temp < reviewText.length(); temp++) {
+      x = (x * 2) + reviewText.charAt(temp);
+      x = x % size;
     }
     return x;
   }
   
-  /*public boolean contains(String s) {
+  public double getAverage(String reviewText) {
+    double x = 2;
+    if (contains(reviewText)) {
+      x = Word[computeHash(reviewText)].getAverage();
+    }
+    return x;
+  }
+  
+  public void put(String reviewText, int score) {
+    if (contains(reviewText)) {
+      Word[computeHash(reviewText)].addNewAppearance(score);
+    } else {
+      Word[computeHash(reviewText)] = new WordEntry(reviewText, score);
+    }
+  }
+  
+  public boolean contains(String s) {
     boolean x = false;
-    for(WordEntry a: wordEntryList) {
-      if (a.getWord().equals(s)){
-        x = true;
-      }
+    if (Word[computeHash(s)] != null) {
+      x = true;
     }
     return x;
-  }*/
-  
-   public double getAverage(String s) {
-    double x = 2.0D;
-    /*if(contains(s)) {
-      getWordEntry(s).getAverage();
-    }
-    return x;*/
-  }
-  
-  public WordEntry getWordEntry(String s) {
-    WordEntry a = null;
-    /*for(WordEntry word: wordEntryList) {
-      if(word.getWord().equals(s)) {
-        a = word;
-      }
-    }*/
-    return a;
-  }
- 
-  public void put(String s, int score) {
-    /*if(contains(s)) {
-      getWordEntry(s).addNewAppearance(score);
-    }else {*/
-      WordEntry a = new WordEntry(s, score);
-      wordEntryList.add(a);
-    }
   }
 }
+
